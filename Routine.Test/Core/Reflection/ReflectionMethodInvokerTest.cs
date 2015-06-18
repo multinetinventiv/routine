868dc58a98c2fa35dc09e531c2cdbd563c3083ce
Cihan Deniz <cih.deniz@gmail.com>
2015-06-18 11:18:01 +0300

    v0.6.11
    - BUGFIX: custom exceptions were causing a streaming exception when trying to preserve the stack trace; fixed.

--- Routine.Test/Core/Reflection/ReflectionMethodInvokerTest.cs
+++ Routine.Test/Core/Reflection/ReflectionMethodInvokerTest.cs
@@ -7,7 +7,7 @@ namespace Routine.Test.Core.Reflection
 	[TestFixture]
 	public class ReflectionMethodInvokerTest
 	{
-		public void Throw(Exception ex)
+		public void Throw(CustomException ex)
 		{
 			throw ex;
 		}
@@ -15,7 +15,7 @@ namespace Routine.Test.Core.Reflection
 		[Test]
 		public void When_exception_occurs_invoker_should_throw_the_actual_exception()
 		{
-			var expected = new Exception("message");
+			var expected = new CustomException("message");
 
 			var testing = new ReflectionMethodInvoker(GetType().GetMethod("Throw"));
 
@@ -33,7 +33,7 @@ namespace Routine.Test.Core.Reflection
 		[Test]
 		public void When_throwing_actual_exception_it_preserves_stack_trace()
 		{
-			var expected = new Exception("message");
+			var expected = new CustomException("message");
 
 			var testing = new ReflectionMethodInvoker(GetType().GetMethod("Throw"));
 
@@ -42,12 +42,17 @@ namespace Routine.Test.Core.Reflection
 				testing.Invoke(this, expected);
 				Assert.Fail("exception not thrown");
 			}
-			catch (Exception actual)
+			catch (CustomException actual)
 			{
 				Console.WriteLine(actual.StackTrace);
 
 				Assert.IsTrue(actual.StackTrace.Contains("ReflectionMethodInvokerTest.Throw"), actual.StackTrace);
 			}
+		}
+
+		public class CustomException : Exception
+		{
+			public CustomException(string message) : base(message) { }
 		}
 	}
 }
--- Routine/Core/Runtime/SystemExtensions.cs
+++ Routine/Core/Runtime/SystemExtensions.cs
@@ -1,19 +1,16 @@
 ﻿using System;
-using System.Runtime.Serialization;
+using System.Reflection;
 
 namespace Routine.Core.Runtime
 {
 	public static class SystemExtensions
 	{
+		//TODO couldn't find a better method, .net 4.5 has a fix for this
 		public static void PreserveStackTrace(this Exception ex)
 		{
-			var ctx = new StreamingContext(StreamingContextStates.CrossAppDomain);
-			var mgr = new ObjectManager(null, ctx);
-			var si = new SerializationInfo(ex.GetType(), new FormatterConverter());
-
-			ex.GetObjectData(si, ctx);
-			mgr.RegisterObject(ex, 1, si);
-			mgr.DoFixups();
+			typeof(Exception).GetMethod("PrepForRemoting",
+				BindingFlags.NonPublic | BindingFlags.Instance)
+				.Invoke(ex, new object[0]);
 		}
 	}
 }
