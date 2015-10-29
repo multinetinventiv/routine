919b6e80335126a6e3f8a63a0298bfe82b7fea3f
cihandeniz <cih.deniz@gmail.com>
2015-10-29 19:36:44 +0200

    UI bugfix
    - BUGFIX: route for an object with view type was not built right; fixed

--- Routine/Ui/MvcExtensions.cs
+++ Routine/Ui/MvcExtensions.cs
@@ -6,11 +6,12 @@ namespace Routine.Ui
 {
 	public static class MvcExtensions
 	{
-		public static string Route(this UrlHelper source, ObjectViewModel model)
+		public static string Route(this UrlHelper source, ObjectViewModel model) { return source.Route(model, false); }
+		public static string Route(this UrlHelper source, ObjectViewModel model, bool ignoreGetAsRoute)
 		{
 			if (model == null) { return null; }
 
-			return source.RouteUrl(model.GetRouteName(), model.GetRouteValues());
+			return source.RouteUrl(model.GetRouteName(ignoreGetAsRoute), model.GetRouteValues(ignoreGetAsRoute));
 		}
 
 		public static string Route(this UrlHelper source, OperationViewModel model)
@@ -20,9 +21,10 @@ namespace Routine.Ui
 				model.GetRouteValues());
 		}
 
-		public static string GetRouteName(this ObjectViewModel source)
+		public static string GetRouteName(this ObjectViewModel source) { return source.GetRouteName(false); }
+		public static string GetRouteName(this ObjectViewModel source, bool ignoreGetAsRoute)
 		{
-			return source.Object.IsNaked ? Constants.MVC_GET_ROUTE_NAME : Constants.MVC_GET_AS_ROUTE_NAME;
+			return source.Object.IsNaked || ignoreGetAsRoute ? Constants.MVC_GET_ROUTE_NAME : Constants.MVC_GET_AS_ROUTE_NAME;
 		}
 
 		private static string GetRouteName(this OperationViewModel source)
@@ -30,21 +32,20 @@ namespace Routine.Ui
 			return source.Object.IsNaked ? Constants.MVC_PERFORM_ROUTE_NAME : Constants.MVC_PERFORM_AS_ROUTE_NAME;
 		}
 
-		public static RouteValueDictionary GetRouteValues(this ObjectViewModel source)
+		public static RouteValueDictionary GetRouteValues(this ObjectViewModel source) { return source.GetRouteValues(false); }
+		public static RouteValueDictionary GetRouteValues(this ObjectViewModel source, bool ignoreViewModelId)
 		{
-			return new RouteValueDictionary(new { id = source.Object.Id, modelId = source.Object.ActualType.Id });
-		}
-
-		public static RouteValueDictionary GetRouteValuesIncludingViewModelId(this ObjectViewModel source)
-		{
-			if (source.Object.IsNaked) { return source.GetRouteValues(); }
+			if (source.Object.IsNaked || ignoreViewModelId)
+			{
+				return new RouteValueDictionary(new { id = source.Object.Id, modelId = source.Object.ActualType.Id });
+			}
 
 			return new RouteValueDictionary(new { id = source.Object.Id, actualModelId = source.Object.ActualType.Id, viewModelId = source.Object.ViewType.Id });
 		}
 
 		public static RouteValueDictionary GetRouteValues(this OperationViewModel source)
 		{
-			var result = source.Target.GetRouteValuesIncludingViewModelId();
+			var result = source.Target.GetRouteValues(false);
 			result.Add("operationModelId", source.Operation.Id);
 			return result;
 		}
@@ -60,7 +61,6 @@ namespace Routine.Ui
 		public static MvcHtmlString Partial(this HtmlHelper htmlHelper, ViewModelBase model, string mode, object viewData) { return htmlHelper.Partial(model, mode, VDD(viewData)); }
 		public static MvcHtmlString Partial(this HtmlHelper htmlHelper, ViewModelBase model, string mode, ViewDataDictionary viewData)
 		{
-
 			return htmlHelper.Partial(model.GetViewName(htmlHelper.ViewContext.Controller.ControllerContext, mode), model, viewData);
 		}
 
