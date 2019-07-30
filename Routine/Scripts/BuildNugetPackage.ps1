615100081da6db060164af328d7a9055c00b30fc
melih.orhan <melih.orhan@yahoo.com.tr>
2019-07-30 10:00:54 +0300

    feat: add nuget build package ps script

---
+++ Routine/Scripts/BuildNugetPackage.ps1
@@ -0,0 +1,9 @@
+$id = "Routine"
+$projectName = "Routine"
+$folderName = "Scripts"
+
+$libFiles = New-Object System.Collections.ArrayList
+$libFiles.Add(@{ assemblyName = "Routine.dll"})
+
+Set-Location $PSScriptRoot
+..\..\Tools\Nuget\BuildNugetPackage.ps1 -id $id -folderName $folderName -projectName $projectName -libFiles $libFiles
