4493d0e409052736218e8f3e283590476d48397a
melih.orhan <melih.orhan@yahoo.com.tr>
2019-08-28 15:21:06 +0300

    add operation search

--- src/Routine/Service/TestApp/vue/src/index.js
+++ src/Routine/Service/TestApp/vue/src/index.js
@@ -1,143 +1,177 @@
 ﻿var app = new Vue({
-	el: '#app',
-	store,
-	computed: {
-		responseHeaders() {
-			return store.state.responseHeaders;
-		},
-		models() {
-			return store.state.models;
-		},
-		headers() {
-			return store.state.headers;
-		},
-		modelsloading() {
-			return store.state.modelsloading;
-		},
-		headersloading() {
-			return store.state.headersloading;
-		},
-		requests() {
-			return store.state.requests;
-		}
-	},
-	data() {
-		return {
-			headersState: true,
-			moduleMenuState: {},
-			modelMenuState: {},
-			modules: [],
-			search: ""
-		}
-	},
-	components: {
-		'parameter': httpVueLoader('$urlbase$/File?path=vue/src/components/parameter.vue'),
-		'json-table': httpVueLoader('$urlbase$/File?path=vue/src/components/json-table.vue')
-	},
-	mounted() {
-		this.$store.dispatch('loadConfiguration');
-		this.$store.dispatch('loadApplicationModel');
-	},
-	methods: {
+        el: '#app',
+        store,
+        computed: {
+                responseHeaders() {
+                        return store.state.responseHeaders;
+                },
+                models() {
+                        return store.state.models;
+                },
+                headers() {
+                        return store.state.headers;
+                },
+                modelsloading() {
+                        return store.state.modelsloading;
+                },
+                headersloading() {
+                        return store.state.headersloading;
+                },
+                requests() {
+                        return store.state.requests;
+                },
+                modules() {
+                        let modules = _.groupBy(_.sortBy(this.models, ['Module']), 'Module');
+                        var newModules = {};
+                        Object.keys(modules).forEach(moduleName => {
+                                var modulesByModuleName = modules[moduleName];
+                                modulesByModuleName.forEach(module => {
+                                        if (module.Operations && module.Operations.length > 0) {
+                                                if (!newModules[moduleName]) {
+                                                        newModules[moduleName] = modulesByModuleName;
+                                                }
+                                        }
+                                });
+                        });
+                        return newModules;
+                },
+                filteredData() {
+                        return Object.keys(this.modules).reduce((a, cKey) => {
+                                const data = this.filterOperations(this.modules[cKey]);
+                                if (data.length) {
+                                        a[cKey] = data;
+                                }
+                                return a;
+                        }, {});
+                }
+        },
+        data() {
+                return {
+                        headersState: true,
+                        moduleMenuState: {},
+                        modelMenuState: {},
+                        search: ""
+                }
+        },
+        components: {
+                'parameter': httpVueLoader('$urlbase$/File?path=vue/src/components/parameter.vue'),
+                'json-table': httpVueLoader('$urlbase$/File?path=vue/src/components/json-table.vue')
+        },
+        mounted() {
+                this.$store.dispatch('loadConfiguration');
+                this.$store.dispatch('loadApplicationModel');
+        },
+        methods: {
+                filterOperations(modules) {
+                        return modules.filter(module => {
+                                return module.Operations.some(operation => {
 
-		showOperation: function (operation) {
-			const request = {
-				name: this.$options.filters.splitCamelCase(this.modelOf(operation).Name) + " - " + this.$options.filters.splitCamelCase(operation.Name),
-				operation: operation,
-				active: false,
-				loading: false,
-				target: {
-					ViewModelId: operation.ModelId,
-					ModelId: !this.modelOf(operation).IsViewModel ? operation.ModelId :
-						this.modelOf(operation).ActualModelIds.length === 1 ? this.modelOf(operation.ModelId).ActualModelIds[0] : ""
-				},
-				data: {},
-				response: null,
-				getUrl: function () {
-					var result = '$urlbase$/' + this.target.ModelId;
+                                        var escapeRegex = (str) => str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+                                        var result = new RegExp("^" + this.search.toLowerCase().split("*").map(escapeRegex).join(".*") + "$").test(operation.Name);
+                                        console.log(result);
 
-					if (!(this.target.Id == undefined) && this.target.Id !== '') {
-						result += '/' + this.target.Id;
-					}
+                                        return operation;
+                                        //return _.includes(operation.Name.toLowerCase(), this.search.toLowerCase());
+                                });
+                        });
+                },
+                showOperation: function (operation) {
+                        const request = {
+                                name: this.$options.filters.splitCamelCase(this.modelOf(operation).Name) + " - " + this.$options.filters.splitCamelCase(operation.Name),
+                                operation: operation,
+                                active: false,
+                                loading: false,
+                                target: {
+                                        ViewModelId: operation.ModelId,
+                                        ModelId: !this.modelOf(operation).IsViewModel ? operation.ModelId :
+                                                this.modelOf(operation).ActualModelIds.length === 1 ? this.modelOf(operation.ModelId).ActualModelIds[0] : ""
+                                },
+                                data: {},
+                                response: null,
+                                getUrl: function () {
+                                        var result = '$urlbase$/' + this.target.ModelId;
 
-					if (this.target.ViewModelId !== this.target.ModelId) {
-						result += '/' + this.target.ViewModelId;
-					}
+                                        if (!(this.target.Id == undefined) && this.target.Id !== '') {
+                                                result += '/' + this.target.Id;
+                                        }
 
-					result += '/' + this.operation.Name;
+                                        if (this.target.ViewModelId !== this.target.ModelId) {
+                                                result += '/' + this.target.ViewModelId;
+                                        }
 
-					return result;
-				},
-				invalidateTargetId: function () {
-					delete this.target.Id;
-				},
-				make: function () {
-					var self = this;
-					this.loading = true;
-					axios.post(this.getUrl(), this.data, { headers: app.headers })
-						.then(response => {
-							self.loading = false;
-							self.response = {};
-							self.response.data = response.data;
-							self.response.headers = {};
-							app.responseHeaders.forEach(function (responseHeader) {
-								self.response.headers[responseHeader] = response.headers[responseHeader];
-							});
-						});
-				}
-			};
-			this.$store.dispatch('addRequest', request);
-			this.activate(request);
-		},
+                                        result += '/' + this.operation.Name;
 
-		activate: function (current, $event, $index) {
-			if ($event != undefined && ($event.which === 2 || ($event.which === 1 && ($event.metaKey || $event.ctrlKey)))) {
-				this.close($index);
-				$event.preventDefault();
-				return;
-			}
-			this.requests.forEach(function (request) {
-				request.active = false;
-			});
+                                        return result;
+                                },
+                                invalidateTargetId: function () {
+                                        delete this.target.Id;
+                                },
+                                make: function () {
+                                        var self = this;
+                                        this.loading = true;
+                                        axios.post(this.getUrl(), this.data, { headers: app.headers })
+                                                .then(response => {
+                                                        self.loading = false;
+                                                        self.response = {};
+                                                        self.response.data = response.data;
+                                                        self.response.headers = {};
+                                                        app.responseHeaders.forEach(function (responseHeader) {
+                                                                self.response.headers[responseHeader] = response.headers[responseHeader];
+                                                        });
+                                                });
+                                }
+                        };
+                        this.$store.dispatch('addRequest', request);
+                        this.activate(request);
+                },
 
-			current.active = true;
-		},
+                activate: function (current, $event, $index) {
+                        if ($event != undefined && ($event.which === 2 || ($event.which === 1 && ($event.metaKey || $event.ctrlKey)))) {
+                                this.close($index);
+                                $event.preventDefault();
+                                return;
+                        }
+                        this.requests.forEach(function (request) {
+                                request.active = false;
+                        });
 
-		close: function (index) {
-			const wasActive = this.requests[index].active;
-			this.requests.splice(index, 1);
+                        current.active = true;
+                },
 
-			if (wasActive && this.requests.length > 0) {
-				this.activate(this.requests[0]);
-			}
-		},
+                close: function (index) {
+                        const wasActive = this.requests[index].active;
+                        this.requests.splice(index, 1);
 
-		modelOf: function (obj) {
-			return modelOf(obj);
-		},
+                        if (wasActive && this.requests.length > 0) {
+                                this.activate(this.requests[0]);
+                        }
+                },
 
-		toggleModuleMenuState: function (module) {
-			this.$set(this.moduleMenuState, module, !this.moduleMenuState[module]);
-		},
 
-		toggleModelMenuState: function (model) {
-			this.$set(this.modelMenuState, model, !this.modelMenuState[model]);
-		},
 
-		getRealMarks: function (marks) {
-			return marks.filter(mark => {
-				return /^(?!__routine).+/g.test(mark);
-			});
-		},
+                modelOf: function (obj) {
+                        return modelOf(obj);
+                },
 
-		getModuleModelsByHasOperations: function (moduleModels) {
-			return moduleModels.filter(moduleModel => {
-				_.filter(moduleModel.Operations, function(operation) {
-					return _.includes(operation.Name, this.search);
-				})
-				return moduleModel.Operations && moduleModel.Operations.length > 0;
-			});
-		}
+                toggleModuleMenuState: function (module) {
+                        this.$set(this.moduleMenuState, module, !this.moduleMenuState[module]);
+                },
 
-	}
+                toggleModelMenuState: function (model) {
+                        this.$set(this.modelMenuState, model, !this.modelMenuState[model]);
+                },
+
+                getRealMarks: function (marks) {
+                        return marks.filter(mark => {
+                                return /^(?!__routine).+/g.test(mark);
+                        });
+                },
+
+                getModuleModelsByHasOperations: function (moduleModels) {
+                        return moduleModels.filter(moduleModel => {
+                                return moduleModel.Operations && moduleModel.Operations.length > 0;
+                        });
+                }
+
+        }
 });
--- src/Routine/Service/TestApp/vue/src/store/index.store.js
+++ src/Routine/Service/TestApp/vue/src/store/index.store.js
@@ -1,90 +1,90 @@
 ﻿window.store = new Vuex.Store({
-	state: {
-		responseHeaders: [],
-		models: [],
-		headers: {},
-		modelsloading: true,
-		headersloading: true,
-		requests: []
-	},
-	actions: {
+        state: {
+                responseHeaders: [],
+                models: [],
+                headers: {},
+                modelsloading: true,
+                headersloading: true,
+                requests: []
+        },
+        actions: {
 
-		loadConfiguration({ commit }) {
+                loadConfiguration({ commit }) {
 
-			axios.get("http://localhost:32805/Service/Configuration").then(response => {
-				let data = response.data;
+                        axios.get("$urlbase$/Configuration").then(response => {
+                                let data = response.data;
 
-				commit('SET_HEADERS_LOADING', false);
+                                commit('SET_HEADERS_LOADING', false);
 
-				let headers = {};
-				data.requestHeaders.forEach(function (requestHeader) {
-					headers[requestHeader] = "";
-				});
+                                let headers = {};
+                                data.requestHeaders.forEach(function (requestHeader) {
+                                        headers[requestHeader] = "";
+                                });
 
-				commit('SET_REQUEST_HEADERS', headers);
-				commit('SET_RESPONSE_HEADERS', data.responseHeaders);
-			});
-		},
+                                commit('SET_REQUEST_HEADERS', headers);
+                                commit('SET_RESPONSE_HEADERS', data.responseHeaders);
+                        });
+                },
 
-		loadApplicationModel({ commit }) {
+                loadApplicationModel({ commit }) {
 
-			axios.get("http://localhost:32805/Service/ApplicationModel").then(response => {
-				let data = response.data;
-				let models = data.Models;
+                        axios.get("$urlbase$/ApplicationModel").then(response => {
+                                let data = response.data;
+                                let models = data.Models;
 
-				commit('SET_MODELS_LOADING', false);
+                                commit('SET_MODELS_LOADING', false);
 
-				models.forEach(function (model) {
-					model.Initializer.ModelId = model.Id;
+                                models.forEach(function (model) {
+                                        model.Initializer.ModelId = model.Id;
 
-					model.Datas.forEach(function (data) {
-						data.ModelId = model.Id;
-					});
+                                        model.Datas.forEach(function (data) {
+                                                data.ModelId = model.Id;
+                                        });
 
-					model.Operations.forEach(function (operation) {
-						operation.ModelId = model.Id;
+                                        model.Operations.forEach(function (operation) {
+                                                operation.ModelId = model.Id;
 
-						operation.Parameters.forEach(function (parameter) {
-							parameter.ModelId = model.Id;
-							parameter.OperationName = operation.Name;
-						});
-					});
-				});
-				commit('SET_APPLICATION_MODELS', models);
-			});
-		},
+                                                operation.Parameters.forEach(function (parameter) {
+                                                        parameter.ModelId = model.Id;
+                                                        parameter.OperationName = operation.Name;
+                                                });
+                                        });
+                                });
+                                commit('SET_APPLICATION_MODELS', models);
+                        });
+                },
 
-		addRequest({ commit }, request) {
-			commit('ADD_REQUEST', request);
-		}
-	},
-	mutations: {
-		SET_REQUEST_HEADERS(state, headers) {
-			state.headers = headers;
-		},
+                addRequest({ commit }, request) {
+                        commit('ADD_REQUEST', request);
+                }
+        },
+        mutations: {
+                SET_REQUEST_HEADERS(state, headers) {
+                        state.headers = headers;
+                },
 
-		SET_RESPONSE_HEADERS(state, responseHeaders) {
-			state.responseHeaders = responseHeaders;
-		},
+                SET_RESPONSE_HEADERS(state, responseHeaders) {
+                        state.responseHeaders = responseHeaders;
+                },
 
-		SET_APPLICATION_MODELS(state, models) {
-			state.models = models;
-		},
+                SET_APPLICATION_MODELS(state, models) {
+                        state.models = models;
+                },
 
-		SET_MODELS_LOADING(state, modelsloading) {
-			state.modelsloading = modelsloading;
-		},
+                SET_MODELS_LOADING(state, modelsloading) {
+                        state.modelsloading = modelsloading;
+                },
 
-		SET_HEADERS_LOADING(state, headersloading) {
-			state.headersloading = headersloading;
-		},
+                SET_HEADERS_LOADING(state, headersloading) {
+                        state.headersloading = headersloading;
+                },
 
-		ADD_REQUEST(state, request) {
-			state.requests.push(request);
-		},
+                ADD_REQUEST(state, request) {
+                        state.requests.push(request);
+                },
 
-		CHANGE_MODELS_LOAIDING(state, modelsloading) {
-			state.modelsloading = modelsloading;
-		}
-	}
+                CHANGE_MODELS_LOAIDING(state, modelsloading) {
+                        state.modelsloading = modelsloading;
+                }
+        }
 });
