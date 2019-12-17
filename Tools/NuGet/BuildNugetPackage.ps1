7cd1b4901544b45e3a0729e75673773c48c73a73
melih.orhan <melih.orhan@yahoo.com.tr>
2019-12-17 11:32:43 +0300

    update appveyor script

--- Tools/NuGet/BuildNugetPackage.ps1
+++ Tools/NuGet/BuildNugetPackage.ps1
@@ -5,7 +5,7 @@
     [string]$version,
     [string]$title,
     [string]$author,
-    [string]$iconUrl = "http://nuget.multinet.com.tr/ResourceFiles/Img/routine-logo.png",
+    [string]$iconUrl = "https://github.com/multinetinventiv/routine/blob/feature/71368-routine-nuget-deployment/docs/routine-logo.png",
     [string]$requireLicenseAcceptance = "true",
     [string]$licenseUrl = "http://routineframework.org",
     [string]$description,
@@ -23,7 +23,7 @@ $projectPath = ($MyInvocation.PSScriptRoot).ToString().Replace("\$folderName",""
 $projectAssemblyInfoPath = "$projectPath\Properties\AssemblyInfo.cs"
 $projectUrl = "http://routineframework.org"
 $dotnetExePath = "dotnet.exe"
-$publishFolderPath = "$PSScriptRoot\Packages"
+$publishFolderPath = ".\Artifacts"
 
 $targetFrameworks = New-Object System.Collections.ArrayList
 $targetFrameworks.Add(@{name = ".NETFramework4.5.2"; shortName = "net452"})
