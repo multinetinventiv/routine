227260d4dcdea878a47274810a94ec20b1b778cc
Ceyhun ÜNLÜ <ceyhun.unlu@multinetinventiv.com>
2021-04-15 11:27:29 +0300

    memorycache is added to webcache

--- src/Routine/Core/Cache/WebCache.cs
+++ src/Routine/Core/Cache/WebCache.cs
@@ -1,31 +1,42 @@
-using System.Web;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Routine.Core.Cache
 {
-	public class WebCache : ICache
-	{
-		public bool Contains(string key)
-		{
-			return HttpRuntime.Cache[key] != null;
-		}
+    public class WebCache : ICache
+    {
+        private readonly MemoryCache Cache;
 
-		public void Add(string key, object value)
-		{
-			HttpRuntime.Cache.Insert(key, value);
-		}
+        public WebCache()
+        {
+            Cache = new MemoryCache(new MemoryCacheOptions
+            {
+                SizeLimit = 1024
+            });
+        }
+        public bool Contains(string key)
+        {
+            var dummyVariable = default(object);
+            return Cache.TryGetValue(key, out dummyVariable);
+        }
 
-		public void Remove(string key)
-		{
-			HttpRuntime.Cache.Remove(key);
-		}
+        public void Add(string key, object value)
+        {
+            Cache.Set(key, value);
+        }
 
-		public object this[string key]
-		{
-			get
-			{
-				return HttpRuntime.Cache[key];
-			}
-		}
-	}
+        public void Remove(string key)
+        {
+            Cache.Remove(key);
+        }
+
+        public object this[string key]
+        {
+            get
+            {
+                var result = default(object);
+                return Cache.TryGetValue(key, out result) ? result : default;
+            }
+        }
+    }
 }
 
