eea2acf22c2cbde46e264793987cc0c1db5be783
Ceyhun ÜNLÜ <ceyhununlu@gmail.com>
2021-04-29 09:17:19 +0300

    angular is removed

--- src/Routine/Service/TestApp/app/app.js
+++
@@ -1,8 +0,0 @@
-﻿angular.module("testapp", ["angular.filter", "ui.router", "ui.bootstrap"]);
-
-String.prototype.splitCamelCase = function () {
-	return this
-		.replace(/([.])/g, ' /')
-		.replace(/([A-Z])/g, ' $1')
-		.replace(/^./, function (str) { return str.toUpperCase(); });
-};
--- src/Routine/Service/TestApp/app/config.router.js
+++
@@ -1,5 +0,0 @@
-﻿angular
-	.module('testapp')
-	.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-		$urlRouterProvider.otherwise("/index");
-	}]);
