f1e3b61e07dac51685bb4038d64e50f86ddf6d23
Cihan Deniz <cih.deniz@gmail.com>
2021-08-19 15:17:29 +0300

    finish core.cache

--- src/Routine/Core/Cache/DictionaryCache.cs
+++ src/Routine/Core/Cache/DictionaryCache.cs
@@ -2,47 +2,36 @@ using System.Collections.Generic;
 
 namespace Routine.Core.Cache
 {
-	public class DictionaryCache : ICache
-	{
-		private readonly Dictionary<string, object> dictionary;
+    public class DictionaryCache : ICache
+    {
+        private readonly Dictionary<string, object> dictionary;
 
-		public DictionaryCache()
-		{
-			dictionary = new Dictionary<string, object>();
-		}
+        public DictionaryCache()
+        {
+            dictionary = new Dictionary<string, object>();
+        }
 
-		public bool Contains(string key)
-		{
-			return dictionary.ContainsKey(key);
-		}
+        public object this[string key] => Contains(key) ? dictionary[key] : null;
+        public bool Contains(string key) => dictionary.ContainsKey(key);
 
-		public void Add(string key, object value)
-		{
-			if(Contains(key))
-			{
-				dictionary[key] = value;
-				return;
-			}
+        public void Add(string key, object value)
+        {
+            if (Contains(key))
+            {
+                dictionary[key] = value;
+                return;
+            }
 
-			dictionary.Add(key, value);
-		}
+            dictionary.Add(key, value);
+        }
 
-		public void Remove(string key)
-		{
-			if(Contains(key))
-			{
-				dictionary.Remove(key);
-			}
-		}
-		public object this[string key]
-		{
-			get
-			{
-				if(!Contains(key)) {return null;}
-
-				return dictionary[key];
-			}
-		}
-	}
+        public void Remove(string key)
+        {
+            if (Contains(key))
+            {
+                dictionary.Remove(key);
+            }
+        }
+    }
 }
 
