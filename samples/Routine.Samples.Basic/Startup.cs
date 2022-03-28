6763633ecb2893c0318fc7c4853fb750bbdace6e
cihandeniz <cihan@gcbrains.com>
2022-03-28 14:16:17 +0300

    add debug config for vim

--- samples/Routine.Samples.Basic/Startup.cs
+++ samples/Routine.Samples.Basic/Startup.cs
@@ -2,8 +2,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
 using System.Diagnostics;
+using System;
 
 namespace Routine.Samples.Basic
 {
