bf6a57380ac1c1c846e371776563cef98892b637
cihandeniz <cihan@gcbrains.com>
2022-09-21 09:43:34 +0300

    add method as property tests

--- src/Routine/Engine/Virtual/MethodAsProperty.cs
+++ src/Routine/Engine/Virtual/MethodAsProperty.cs
@@ -9,12 +9,12 @@ public class MethodAsProperty : IProperty
     public MethodAsProperty(IMethod method, params object[] parameters) : this(method, string.Empty, parameters) { }
     public MethodAsProperty(IMethod method, string ignorePrefix, params object[] parameters)
     {
-        if (ignorePrefix == null) { throw new ArgumentNullException(nameof(ignorePrefix)); }
+        if (method == null) { throw new ArgumentNullException(nameof(method)); }
         if (method.Parameters.Count != parameters.Length) { throw new ArgumentException("Given parameters and method parameters do not match"); }
         if (method.ReturnsVoid()) { throw new ArgumentException("Given method must have a return type"); }
 
         this.method = method;
-        this.ignorePrefix = ignorePrefix;
+        this.ignorePrefix = ignorePrefix ?? string.Empty;
         this.parameters = parameters;
     }
 
