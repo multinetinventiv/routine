e74b9cdc681ae8c960f4241d5f95bbad08dee033
alptuncs <alptuncsever@gmail.com>
2022-11-21 12:56:39 +0300

    refactor field names in samples

--- samples/Routine.Samples.SmartObject/HelloWorld/Users.cs
+++ samples/Routine.Samples.SmartObject/HelloWorld/Users.cs
@@ -2,10 +2,10 @@
 
 public class Users : IFinder
 {
-    internal static readonly List<User> users = new();
+    internal static readonly List<User> _users = new();
 
-    public void Add(string name) => users.Add(new User(name));
-    public User Find(string name) => users.FirstOrDefault(u => u.Name == name);
+    public void Add(string name) => _users.Add(new User(name));
+    public User Find(string name) => _users.FirstOrDefault(u => u.Name == name);
 
     object IFinder.Find(string name) => Find(name);
 }
