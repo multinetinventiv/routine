7fe566efe6ef34a042cda0f78982cbc3e1669074
Sefer Mirza <36925434+SeferMirza@users.noreply.github.com>
2023-04-19 14:38:30 +0300

    switch to using constraint model in Delegate Based Convention  tests

--- test/Routine.Test/Core/Configuration/Conventions/DelegateBasedConventionTest.cs
+++ test/Routine.Test/Core/Configuration/Conventions/DelegateBasedConventionTest.cs
@@ -12,17 +12,17 @@ public class DelegateBasedConventionTest : CoreTestBase
             new DelegateBasedConvention<object, string>()
                 .Return(o => o.ToString());
 
-        Assert.AreEqual("1", testing.Apply(1));
+        Assert.That(testing.Apply(1), Is.EqualTo("1"));
     }
 
     [Test]
     public void When_no_delegate_was_given_returns_default_value()
     {
         IConvention<int, string> testing1 = new DelegateBasedConvention<int, string>();
-        Assert.AreEqual(null, testing1.Apply(1));
+        Assert.That(testing1.Apply(1), Is.EqualTo(null));
 
         IConvention<string, int> testing2 = new DelegateBasedConvention<string, int>();
-        Assert.AreEqual(0, testing2.Apply("string"));
+        Assert.That(testing2.Apply("string"), Is.EqualTo(0));
     }
 
     [Test]
@@ -30,7 +30,7 @@ public class DelegateBasedConventionTest : CoreTestBase
     {
         IConvention<string, string> testing = BuildRoutine.Convention<string, string>().Constant("constant_result");
 
-        Assert.AreEqual("constant_result", testing.Apply("test1"));
-        Assert.AreEqual("constant_result", testing.Apply("test2"));
+        Assert.That(testing.Apply("test1"), Is.EqualTo("constant_result"));
+        Assert.That(testing.Apply("test2"), Is.EqualTo("constant_result"));
     }
 }
