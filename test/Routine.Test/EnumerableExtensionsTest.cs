c05d393e1db03d0171f9cacb5a7d0d9260185c74
Sefer Mirza <36925434+SeferMirza@users.noreply.github.com>
2023-04-20 11:14:29 +0300

    switch to using constraint model in other tests

--- test/Routine.Test/EnumerableExtensionsTest.cs
+++ test/Routine.Test/EnumerableExtensionsTest.cs
@@ -6,31 +6,31 @@ public class EnumerableExtensionsTest
     [Test]
     public void Test_IEnumerable_ToItemString()
     {
-        Assert.AreEqual("[a,b]", new[] { "a", "b" }.ToItemString());
+        Assert.That(new[] { "a", "b" }.ToItemString(), Is.EqualTo("[a,b]"));
     }
 
     [Test]
     public void Test_IEnumerable_ItemEquals()
     {
-        Assert.IsTrue(((IEnumerable)null).ItemEquals(null));
-        Assert.IsFalse(((IEnumerable)null).ItemEquals(Array.Empty<string>()));
-        Assert.IsFalse(Array.Empty<string>().ItemEquals(null));
+        Assert.That(((IEnumerable)null).ItemEquals(null), Is.True);
+        Assert.That(((IEnumerable)null).ItemEquals(Array.Empty<string>()), Is.False);
+        Assert.That(Array.Empty<string>().ItemEquals(null), Is.False);
 
-        Assert.IsTrue(Array.Empty<string>().ItemEquals(new List<string>()));
-        Assert.IsTrue(Array.Empty<object>().ItemEquals(new List<string>()));
+        Assert.That(Array.Empty<string>().ItemEquals(new List<string>()), Is.True);
+        Assert.That(Array.Empty<object>().ItemEquals(new List<string>()), Is.True);
 
-        Assert.IsTrue(new[] { "a" }.ItemEquals(new List<string> { "a" }));
+        Assert.That(new[] { "a" }.ItemEquals(new List<string> { "a" }), Is.True);
 
-        Assert.IsFalse(new[] { "a" }.ItemEquals(new List<string> { "a", "b" }));
-        Assert.IsFalse(new[] { "a", "b" }.ItemEquals(new List<string> { "a" }));
+        Assert.That(new[] { "a" }.ItemEquals(new List<string> { "a", "b" }), Is.False);
+        Assert.That(new[] { "a", "b" }.ItemEquals(new List<string> { "a" }), Is.False);
     }
 
     [Test]
     public void Test_IEnumerable_GetItemHashCode()
     {
-        Assert.AreEqual(Array.Empty<string>().GetItemHashCode(), new List<string>().GetItemHashCode());
+        Assert.That(new List<string>().GetItemHashCode(), Is.EqualTo(Array.Empty<string>().GetItemHashCode()));
 
-        Assert.AreEqual(new[] { "a" }.GetItemHashCode(), new List<string> { "a" }.GetItemHashCode());
-        Assert.AreNotEqual(new[] { "a" }.GetItemHashCode(), new List<string> { "a", "b" }.GetItemHashCode());
+        Assert.That(new List<string> { "a" }.GetItemHashCode(), Is.EqualTo(new[] { "a" }.GetItemHashCode()));
+        Assert.That(new List<string> { "a", "b" }.GetItemHashCode(), Is.Not.EqualTo(new[] { "a" }.GetItemHashCode()));
     }
 }
